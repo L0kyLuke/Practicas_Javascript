cb9cb31030a549113e0220a1245d9269a63bb5cc
L0kyLuke <boss.aerialist@gmail.com>
2022-11-24 01:29:19 +0100

    fin

--- Modulo_5/array.js
+++ Modulo_5/array.js
@@ -49,65 +49,124 @@ const carrito = [
         premium: true
     }
 ];
-// Eliminar el segundo elemento del carro
-function eliminado(carrito) {
-    var elementoEliminado = carrito.splice(1, 1);
-}
-eliminado(carrito);
 
-// Listamos todos los productos
-function imprimir(carro) {
-    console.log("------------------");
-    for (attr in carro) {
-        console.log(attr.toUpperCase() + ": " + carro[attr]);
-    }
-}
-for (carro of carrito) {
-    imprimir(carro);
-}
-
-// Calcular total de la compra
-function calcula(carrito) {
-    let total = 0;
+//Mostrar todos los elementos del carro
+function carga() {
+    var main = document.getElementById('productos');
+    var i = 0;
     for (carro of carrito) {
-        total += carro.price * carro.count;
-    }
-    if (total < 50) {
-        console.log("------------------");
-        console.log("---PRECIO TOTAL---");
-        console.log("------------------");
-        console.log(total.toFixed(3) + "€");
-    } else {
-        total = total-total*0.05;
-        console.log("--------------------------------------");
-        console.log("---PRECIO TOTAL CON 5% DE DESCUENTO---");
-        console.log("--------------------------------------");
-        console.log(total.toFixed(3) + "€");
-    }
-}
-calcula(carrito);
+        var elemento = document.createElement('li');
+        var nombre = document.createElement('h2');
+        var precio = document.createElement('h3');
+        var premium = document.createElement('h4');
+        var identificador = document.createElement('h4');
+        var cantidad = document.createElement('h5');
+        var linea = document.createElement('hr');
 
-// Filtrar productos prime
-function filtrar(carro) {
-    console.log("------------------");
-    for (attr in carro) {
-        console.log(attr.toUpperCase() + ": " + carro[attr]);
-    }
-}
-document.getElementById("prime").onclick = function checkPrime() {
-    console.clear();
-    if (document.getElementById("prime").checked == true) {
-        for (carro of carrito) {
-            if (carro.premium) filtrar(carro);
+
+        elemento.appendChild(nombre);
+        elemento.appendChild(identificador);
+        elemento.appendChild(precio);
+        elemento.appendChild(premium);
+        elemento.appendChild(cantidad);
+
+
+        if (document.getElementById("prime").checked == true) {
+
+            if (carro.premium == true) {
+                main.appendChild(elemento);
+                nombre.innerHTML = carro.name;
+                identificador.innerHTML = "ID: " + carro.id;
+                precio.innerHTML = carro.price + "€";
+                cantidad.innerHTML = "Cantidad: " + carro.count;
+                premium.innerHTML = "<span style='color:orange'>Producto Premium</span>";
+                var elimina = document.createElement('button');
+                elimina.innerHTML = "Eliminar";
+                elimina.setAttribute("id", carro.id);
+                elemento.appendChild(elimina);
+                elimina.addEventListener("click", eliminaElemento);
+                elemento.appendChild(linea);
+
+            }
+        } else {
+            main.appendChild(elemento);
+            nombre.innerHTML = carro.name;
+            identificador.innerHTML = "ID: " + carro.id;
+            precio.innerHTML = carro.price + "€";
+            cantidad.innerHTML = "Cantidad: " + carro.count;
+            if (carro.premium == true) {
+                premium.innerHTML = "<span style='color:orange'>Producto Premium</span>";
+            }
+            var elimina = document.createElement('button');
+            elimina.innerHTML = "Eliminar";
+            elimina.setAttribute("id", carro.id);
+            elemento.appendChild(elimina);
+            elimina.addEventListener("click", eliminaElemento);
+            elemento.appendChild(linea);
         }
-        console.log("------------------");
-        console.log("Gastos de envío cero");
-    } else {
-        for (carro of carrito) {
-            imprimir(carro);
-        }
-        console.log("------------------");
-        console.log("Con gastos de envío");
+
     }
     calcula(carrito);
 }
+
+carga();
+
+// Calcular total de la compra
+function calcula(carrito) {
+    var noPrime = 0;
+    let total = 0;
+    var result = document.getElementById('total');
+    var res = document.createElement('h1');
+    var envio = document.createElement('h3');
+    result.appendChild(res);
+    for (carro of carrito) {
+        total += carro.price * carro.count;
+        if(carro.premium==false){//Contador de elementos no prime en carro
+            noPrime++;
+        }
+    }
+    //Mostrar descuento si la compra es superior a 50
+    if (total < 50) {
+        res.innerHTML = "Precio total: " + total.toFixed(2) + "€";
+    } else {
+        total = total - total * 0.05;
+        res.innerHTML = "Precio total con 5% de descuento: " + total.toFixed(2) + "€";
+    }
+
+
+    //Mostrar tipo de gastos de envío dependiendo de si hay elementos no prime en carro
+    result.appendChild(envio);
+    if (noPrime==0 && carrito.length>0) {
+        envio.innerHTML = "<span style='color:green'>Gastos de envío gratis</span>";
+    }
+    else if(carrito.length>0) {
+        envio.innerHTML = "<span style='color:red'>Con gastos de envío</span>";
+    }
+}
+
+//Para mostrar solo los primes o todos
+document.getElementById("prime").onclick = function checkPrime() {
+    document.getElementById("productos").innerHTML = "";
+    document.getElementById("total").innerHTML = "";
+    carga();
+}
+
+//Elimina el elemento que pulsemos
+function eliminaElemento() {
+    var buttons = document.getElementsByTagName("button");
+    var buttonsCount = buttons.length;
+    var y = 0;
+    for (var i = 0; i <= buttonsCount; i += 1) {
+
+        while (carrito[y].id != this.id) {
+            y++
+        }
+        carrito.splice(y, 1);
+        document.getElementById("productos").innerHTML = "";
+        document.getElementById("total").innerHTML = "";
+        carga();
+
+    }
+}
+
+
